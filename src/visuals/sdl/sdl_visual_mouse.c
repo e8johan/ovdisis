62ef16176b6742064ad9b294472774d29b9970c6
Johan Thelin <e8johan@gmail.com>
2015-08-24 20:32:20 +0200

    Implemented mouse background store/restore

--- src/visuals/sdl/sdl_visual_mouse.c
+++ src/visuals/sdl/sdl_visual_mouse.c
@@ -14,10 +14,15 @@
 
 #define DEBUGLEVEL 0
 
+#include <SDL/SDL.h>
 #include <ovdisis.h>
 
+#include "sdl_visual.h"
 #include "sdl_visual_mouse.h"
 
+static int x_last = 0;
+static int y_last = 0;
+static unsigned char mouse_background_buffer[16*16*4];
 
 /*
 ** Description
@@ -28,7 +33,23 @@ sdl_visual_save_mouse_bg(void * fb,
                          int    x,
                          int    y)
 {
-  /* DEBUG0("Implement sdl_visual_restore_mouse_bg\n"); */
+  int iy;
+  unsigned char *ptr = mouse_background_buffer;
+  unsigned char *pixel;
+  int stride;
+  SDL_Surface * screen = VISUAL_T(fb);
+  
+  x_last = x;
+  y_last = y;
+  
+  stride = 16*screen->format->BytesPerPixel;
+  
+  for(iy = 0; iy<16; iy++)
+  {
+    pixel = (unsigned char*)(screen->pixels + (iy+y)*screen->pitch + x*screen->format->BytesPerPixel);
+    memcpy(ptr, pixel, stride);
+    ptr += stride;
+  }
 }
 
 
@@ -39,5 +60,34 @@ sdl_visual_save_mouse_bg(void * fb,
 void
 sdl_visual_restore_mouse_bg(void * fb)
 {
-  /* DEBUG0("Implement sdl_visual_restore_mouse_bg\n"); */
+  int iy;
+  unsigned char *ptr = mouse_background_buffer;
+  unsigned char *pixel;
+  int stride;
+  SDL_Surface * screen = VISUAL_T(fb);
+
+  if(SDL_MUSTLOCK(screen))
+  {
+    if(SDL_LockSurface(screen) < 0)
+    {
+      printf("COULD NOT LOCK\n");
+      return;
+    }
+  }
+
+  stride = 16*screen->format->BytesPerPixel;
+  
+  for(iy = 0; iy<16; iy++)
+  {
+    pixel = (unsigned char*)(screen->pixels + (iy+y_last)*screen->pitch + x_last*screen->format->BytesPerPixel);
+    memcpy(pixel, ptr, stride);
+    ptr += stride;
+  }
+  
+  if(SDL_MUSTLOCK(screen))
+  {
+    SDL_UnlockSurface(screen);
+  }
+
+  SDL_UpdateRect(screen, x_last, y_last, 16, 16);
 }
